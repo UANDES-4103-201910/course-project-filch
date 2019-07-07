bc64c6de573e6e3f6fa5ca18785c8fc2bf2ffd83
nozeback <ajaltermatt@miuandes.cl>
2019-07-07 11:51:01 -0400

    dandole

--- filch/app/models/rant.rb
+++ filch/app/models/rant.rb
@@ -2,7 +2,7 @@ class Rant < ApplicationRecord
   belongs_to :user
   has_one :geofence
   has_many :comments, dependent: :delete_all
-  belong_to :dumpster, :class_name => 'Dumpster',optional: true
+  belongs_to :dumpster, :class_name => 'Dumpster',optional: true
   has_many_attached :images
   has_one :platform
 
--- filch/app/views/users/_user.json.jbuilder
+++ filch/app/views/users/_user.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! user, :id, :name, :lastname, :geofence_id, :role, :uid, :provider, :sex, :bigraphy, :created_at, :updated_at
+json.extract! user, :id, :name, :lastname, :geofence_id, :role, :uid, :provider, :sex, :biography, :created_at, :updated_at
 json.url user_url(user, format: :json)
